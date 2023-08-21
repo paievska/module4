f6761e450c62de007e50a5fec459251b357d511c
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-08-22 00:29:10 +0300

    add file package-info.java

---
+++ src/main/java/org/example/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains classes related to string transformation and logging.
+ * It includes the main application class, the logging aspect, and JMX MBean interfaces.
+ * You can use these classes to transform strings and control logging using JMX.
+ */
+package org.example;
